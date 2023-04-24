85d2c476764aed1ae40eb01b11adf5ebcbc4a543
Glsme <glasses.str.man@gmail.com>
2023-04-24 12:47:54 +0900

    Feat: 초기 설정

--- TCA-CleanArchitecture_Example/Application/SceneDelegate.swift
+++ TCA-CleanArchitecture_Example/Application/SceneDelegate.swift
@@ -5,6 +5,7 @@
 //  Created by Seokjune Hong on 2023/04/24.
 //
 
+import SwiftUI
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
@@ -16,7 +17,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let scene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: scene)
+        window?.rootViewController = UIHostingController(rootView: ContentView())
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
---
+++ TCA-CleanArchitecture_Example/Presentation/View/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  TCA-CleanArchitecture_Example
+//
+//  Created by Seokjune Hong on 2023/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
